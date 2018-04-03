a233dd0228c6e52841a45c9106f6e739f3a61979
Kiera <kiera.thomasson@gmail.com>
2018-04-03 12:31:59 -0700

    Generated passenger controller and model. Updated controller methods, and routes

---
+++ RideShare/app/controllers/passengers_controller.rb
@@ -0,0 +1,66 @@
+class PassengersController < ApplicationController
+  def index
+
+    @passengers = Passenger.all
+
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    passenger = Passenger.new(passenger_params)
+
+    if passenger.save
+      redirect_to passengers_path
+    end
+  end
+
+  def show
+    passenger_id = params[:id]
+
+    @passenger = Passenger.find(passenger_id)
+  end
+
+  def edit
+    @passenger = Passenger.find(params[:id])
+  end
+
+  def update
+    passenger = Passenger.find(params[:id])
+
+    passenger.assign_attributes(passenger_params)
+
+    if passenger.save
+      redirect_to passenger_path(passenger)
+    end
+  end
+
+  def destroy
+    Passenger.destroy(params[:id])
+
+    redirect_to passengers_path
+  end
+
+  # def complete
+  #   passenger = Task.find(params[:id])
+  #   passenger.completion_date = DateTime.now.strftime("%a, %B %d, %Y")
+  #
+  #   if passenger.status == "complete"
+  #     passenger.assign_attributes(status: "not complete", completion_date: nil)
+  #   else
+  #     passenger.assign_attributes(status: "complete")
+  #   end
+  #
+  #   if passenger.save
+  #     redirect_to passengers_path
+  #   end
+  # end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+end
--- RideShare/config/routes.rb
+++ RideShare/config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
 resources :trips
 
+resources :passengers
+
 end
