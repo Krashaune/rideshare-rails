d8a42d8b069da8c74431c5ecb0c1ceb93c165a4f
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-03 12:16:04 -0700

    Generated and updated the trip controller and routes. Generated the trip model and database.

---
+++ RideShare/app/controllers/trips_controller.rb
@@ -0,0 +1,68 @@
+class TripsController < ApplicationController
+
+  def index
+
+    @trips = Trip.all
+
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+
+    if trip.save
+      redirect_to trips_path
+    end
+  end
+
+  def show
+    trip_id = params[:id]
+
+    @trip = Trip.find(trip_id)
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+
+    trip.assign_attributes(trip_params)
+
+    if trip.save
+      redirect_to trip_path(trip)
+    end
+  end
+
+  def destroy
+    Trip.destroy(params[:id])
+
+    redirect_to trips_path
+  end
+
+  # def complete
+  #   trip = Task.find(params[:id])
+  #   trip.completion_date = DateTime.now.strftime("%a, %B %d, %Y")
+  #
+  #   if trip.status == "complete"
+  #     trip.assign_attributes(status: "not complete", completion_date: nil)
+  #   else
+  #     trip.assign_attributes(status: "complete")
+  #   end
+  #
+  #   if trip.save
+  #     redirect_to trips_path
+  #   end
+  # end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost)
+  end
+
+end
