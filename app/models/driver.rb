3694a98e7112f9468fafde71ed5a3b57368a7052
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-04-05 19:27:45 -0700

    Added the total_revenue and average rating method to the driver.rb. Updated the driver show page with the info such as average rating and total revenue. Updated the index page to include the average rating.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,31 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  def total_revenue
+    fee = 1.65
+    driver_takehome = 0.8
+
+    subtotal = 0
+    trips.each do |trip|
+      subtotal += trip.cost - fee
+    end
+
+    total = subtotal * driver_takehome
+    return (total/100).round(2)
+  end
+
+  def average_rating
+    total_ratings = 0
+    trips.each do |trip|
+      total_ratings += trip.rating
+    end
+
+    if trips.length == 0
+      average = 0
+    else
+      average = (total_ratings.to_f) / trips.length
+    end
+
+    return average
+  end
 end
